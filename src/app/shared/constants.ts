b4ecedad85a7a0fa95635d6e5656606e4e525d98
DvirMon <dmenajem@gmail.com>
2023-05-19 15:33:07 +0300

    feat () - deploy to firebase works

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDUkamHvDo5RnEILnGSpPBxYieLaSMLAOA",
+  authDomain: "coincryptohub.firebaseapp.com",
+  projectId: "coincryptohub",
+  storageBucket: "coincryptohub.appspot.com",
+  messagingSenderId: "524112368876",
+  appId: "1:524112368876:web:11544e7663138d7ef381c9"
+};
