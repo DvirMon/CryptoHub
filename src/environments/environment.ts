11adc72410ca0230cab22ba93000dfea2ccdb794
DvirMon <dmenajem@gmail.com>
2023-05-19 13:28:19 +0300

    feat (env) -  update server url to railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  server : "https://web-production-03ab.up.railway.app"
-  // server : "http://localhost:3000"
-  // server : "https://crypto-coins-server.herokuapp.com"
+  server : "http://localhost:3000"
 };
 
