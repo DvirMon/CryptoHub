f646c4c9455e50a24df46ccbb68136090255488e
dvir.monajem <dmenajem@gmail.com>
2024-02-28 10:12:31 +0200

    feat (environment) - update dev to railway url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // server : "https://web-production-03ab.up.railway.app"
-  server : "http://localhost:3000"
+  server : "https://web-production-03ab.up.railway.app"
 };
 
